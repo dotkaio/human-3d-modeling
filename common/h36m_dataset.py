51292e7a2a6e8c91eec39696d6a7584e91b2d1ea
kaio ferraz <72061085+dotkaio@users.noreply.github.com>
2024-12-21 00:00:03 -0800

    Update h36m_dataset.py

--- common/h36m_dataset.py
+++ common/h36m_dataset.py
@@ -16,6 +16,8 @@ h36m_skeleton = Skeleton(parents=[-1,  0,  1,  2,  3,  4,  0,  6,  7,  8,  9,  0
        joints_left=[6, 7, 8, 9, 10, 16, 17, 18, 19, 20, 21, 22, 23],
        joints_right=[1, 2, 3, 4, 5, 24, 25, 26, 27, 28, 29, 30, 31])
 
+
+# TODO:include new call method for api!
 h36m_cameras_intrinsic_params = [
     {
         'id': '54138969',
